9635b05f35f9d105f5f115b60383dcba75b013e1
不完美小孩 <49219172+bytehandsome@users.noreply.github.com>
2022-11-14 11:47:42 +0800

    增加等待“提交按钮"

--- jksb_sysu.py
+++ jksb_sysu.py
@@ -88,6 +88,7 @@ def jksb(driver):
     driver.find_element_by_xpath('//*[@id="form_command_bar"]/li[1]').click()
 
     wait.until(expected_conditions.element_to_be_clickable((By.XPATH, "//*[@id='form_command_bar']/li[2]")) ) # 出现终止按钮
+    wait.until(expected_conditions.text_to_be_present_in_element((By.XPATH, "//*[@id='form_command_bar']/li[1]"), '提交'))
     logging.info("提交健康申报")
     driver.find_element_by_xpath('//*[@id="form_command_bar"]/li[1]').click()
 
